bb0d53afb867402234a1038e89c19b782e0f2895
Nikolay Ermolovich <rupreht@rupreht.ru>
2021-09-06 21:49:16 +0500

    - add secrets

---
+++ files/mk-sensitive-dir.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# file /usr/local/sbin/mk-sensitive-dir.sh
+umask 0077
+
+for D in `awk -F':' '{if ($3>999 && $3<10000) print $1}' /etc/passwd`; do
+	mkdir -p -m 0700 /run/secrets-${D}
+	touch /run/secrets-${D}/MYSQL_USER /run/secrets-${D}/MYSQL_PASSWORD /run/secrets-${D}/MYSQL_DATABASE
+	chown -R ${D}:0 /run/secrets-${D}
+done
+exit 0
