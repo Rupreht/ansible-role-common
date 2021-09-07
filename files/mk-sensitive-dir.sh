95c26feed6bdbb5bede4fe7efab9aa7c3f0a3b94
Nikolay Ermolovich <rupreht@rupreht.ru>
2021-09-07 14:18:56 +0500

    - fix

--- files/mk-sensitive-dir.sh
+++ files/mk-sensitive-dir.sh
@@ -7,4 +7,5 @@ for D in `awk -F':' '{if ($3>999 && $3<10000) print $1}' /etc/passwd`; do
 	touch /run/secrets-${D}/MYSQL_USER /run/secrets-${D}/MYSQL_PASSWORD /run/secrets-${D}/MYSQL_DATABASE
 	chown -R ${D}:0 /run/secrets-${D}
 done
+[ -x /root/bin/dp-secrets.sh ] && /root/bin/dp-secrets.sh
 exit 0
